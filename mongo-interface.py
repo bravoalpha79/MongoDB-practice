28f3ec9f405ca8181c4a1c88ffe27489888d013d
Igor Basuga <igor.basuga@gmail.com>
2020-03-02 19:31:48 +0000

    add gitignore

---
+++ mongo-interface.py
@@ -0,0 +1,17 @@
+import pymongo
+import os
+if os.path.exists("env.py"):
+  import env 
+
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "myTestDB"
+COLLECTION_NAME = "myFirstMDB"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
